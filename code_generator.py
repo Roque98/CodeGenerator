1aa92d33270ce938d0fec3ca43b47fab253bec13
Angel David Roque Ayala <ingroque98@gmail.com>
2024-08-17 20:22:06 -0600

    Se agregan metodos para generar entidad y model

--- code_generator.py
+++ code_generator.py
@@ -53,7 +53,7 @@ def generate_code_response(model: ChatOpenAI, result: str):
     # Parsear la salida para convertirla en una instancia de CodeResponseResult
     return parser.invoke(output)
 
-def generate_sps(model: ChatOpenAI, script_table: str) -> str :
+def generate_sps(model: ChatOpenAI, script_table: str) -> CodeResponseResult :
     
     promptTemplate = PromptTemplate.from_template(
         """
@@ -79,6 +79,8 @@ def generate_sps(model: ChatOpenAI, script_table: str) -> str :
             -- Create date: {date}
             -- Description:	<Description,,>
             -- =============================================
+
+            El sp _GetAll debe recibir al final como parametros opcionales fechaInicio y fechaFin para poder delimitar por fecha los resultados
         """
     )
 
@@ -89,26 +91,140 @@ def generate_sps(model: ChatOpenAI, script_table: str) -> str :
     output = prompt_and_model.invoke({"script_table": script_table, "date": datetime.now()})
 
     # Parsear la salida para convertirla en una instancia de CodeResponseResult
-    return output.content
+    return generate_code_response(model, clean_code_mll_generated(output.content))
 
-def clean_mll_generated_sql(text):
+def test_sps(model: ChatOpenAI):
+    script_table = """
+        USE [catalogos]
+        GO
+
+        CREATE TABLE [dbo].[prtgDevices](
+            [idPrtg] [int] NULL,
+            [idObj] [int] NULL,
+            [device] [varchar](50) NULL,
+            [host] [varchar](50) NULL,
+            [tags] [varchar](50) NULL
+        ) ON [PRIMARY]
+        GO
     """
-    Limpia un texto generado por un modelo de lenguaje (MLL) eliminando los símbolos extra y dejando solo el código SQL.
+
+    return generate_sps(model, script_table)
+
+def generate_entidad_coms(model: ChatOpenAI, script_table: str) -> CodeResponseResult:
+    promptTemplate = PromptTemplate.from_template(
+        """
+            Eres un generador de codigo c#. La salida que generes sera guardada en un archivo por lo que no debes generar ningun texto adicional ademas del codigo. No agregues ningun caracter adicional o el compilador C# fallara
+            
+            Tomando en cuenta la siguiente documentacion de los tabla
+            
+            {script_table} 
+            
+            Genera un archivo que contenga el codigo de la clase para guardar los datos de la tabla llamado [nombreEntidad]Entidad.cs
+            
+            Remplaza el texto [nombreEntidad] por un nombre adecuado tomando en cuenta el nombre de la tabla.
+        """
+    )
+
+    # Combinar el prompt con el modelo de lenguaje
+    prompt_and_model = promptTemplate | model
+
+    # Ejecutar el modelo con la consulta y obtener la salida
+    output = prompt_and_model.invoke({"script_table": script_table})
+
+    # Parsear la salida para convertirla en una instancia de CodeResponseResult
+    return generate_code_response(model, clean_code_mll_generated(output.content))
+
+def test_entidad(model: ChatOpenAI):
+    script_table = """
+        USE [catalogos]
+        GO
+
+        CREATE TABLE [dbo].[prtgDevices](
+            [idPrtg] [int] NULL,
+            [idObj] [int] NULL,
+            [device] [varchar](50) NULL,
+            [host] [varchar](50) NULL,
+            [tags] [varchar](50) NULL
+        ) ON [PRIMARY]
+        GO
+    """
+
+    result = clean_code_mll_generated(generate_entidad_coms(model, script_table))
+    print(result)
+    return generate_code_response(model, result)
+
+def generate_model_coms(model: ChatOpenAI, documentation: str) -> CodeResponseResult:
+    framgento_codigo = """
+        namespace Consola_Monitoreo.Models
+        {
+            public class BaseDatosModel : Model
+            {
+                public BaseDatosModel() : base()
+                {
+
+                }
+
+                public List<AlertaTablespace> GetAll(DateTime fechaInicio, DateTime fechaFin)
+                {
+                    return ejecutaStoredProcedure<AlertaTablespace>("ConsolaMonitoreo.dbo.BDOracle_ConsultaAlertasTablespace", new Dictionary<string, object>()
+                    {
+                        {"@fechaInicio", fechaInicio },
+                        {"@fechaFin", fechaFin }
+                    });
+                }
+
+            }
+        }
+    """
+    
+    promptTemplate = PromptTemplate.from_template(
+        """
+            Eres un generador de codigo c#. La salida que generes sera guardada en un archivo por lo que no debes generar ningun texto adicional ademas del codigo. No agregues ningun caracter adicional o el compilador C# fallara
+            
+            Tomando en cuenta la siguiente documentacion de los sp y la clase entidad
+            
+            {documentation} 
+            
+            Genera un archivo que contenga el codigo de la clase para model llamado [nombreEntidad]Model.cs
+
+            Esto tomando como referencia la siguiente clase para que sigas el mismo estilo de codigo.
+
+            {framgento_codigo}
+            
+            Asegurante de implementar todos los metodos para todos los sps. Documenta las funciones c#
+            
+
+            Remplaza el texto [nombreEntidad] por un nombre adecuado tomando en cuenta el nombre de la tabla.
+        """
+    )
+
+    # Combinar el prompt con el modelo de lenguaje
+    prompt_and_model = promptTemplate | model
+
+    # Ejecutar el modelo con la consulta y obtener la salida
+    output = prompt_and_model.invoke({"documentation": documentation, "framgento_codigo": framgento_codigo})
+
+    # Parsear la salida para convertirla en una instancia de CodeResponseResult
+    return generate_code_response(model, clean_code_mll_generated(output.content))
+
+
+def clean_code_mll_generated(code):
+    """
+    Limpia un texto generado por un modelo de lenguaje (MLL) eliminando los símbolos extra y dejando solo el código de programacion.
     
     :param text: El texto generado por el MLL.
     :return: Un string con el código SQL limpio.
     """
     # Reemplazar las secuencias no necesarias
-    cleaned_text = text.replace("\\n", "\n")\
+    code_text = code.replace("\\n", "\n")\
         .replace("GO\n", "")\
         .replace("SET NOCOUNT ON;", "")\
         .replace("```", "")\
         .strip()
     
-    return cleaned_text
+    return code_text
 
 if __name__ == '__main__':
-
     model =ChatOpenAI(model="gpt-4o-mini")
 
     script_table = """
@@ -125,7 +241,13 @@ if __name__ == '__main__':
         GO
     """
 
-    result = clean_mll_generated_sql(generate_sps(model, script_table))
-    resultClass = generate_code_response(model, result)
-    print(result)
+    result_entidad = generate_entidad_coms(model,script_table)
+    result_sps = generate_sps(model,script_table)
 
+    documentation = result_entidad.documentation + result_sps.documentation
+
+    result_model = generate_model_coms(model, documentation)
+    print(result_model)
+    
+
+    
